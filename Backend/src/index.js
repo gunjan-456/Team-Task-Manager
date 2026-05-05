7c661a616526b940c010f06737914a670e97be14
Gunjan Singh <gunjansingh72002@gmail.com>
2026-05-05 15:38:14 +0530

    commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -8,7 +8,10 @@ connectDB()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin:  process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json())
 
 app.use("/api/auth", require("./routes/authRoutes"))
@@ -16,4 +19,6 @@ app.use("/api/projects", require("./routes/projectRoutes"))
 app.use("/api/tasks", require("./routes/taskRoutes"))
 app.use("/api/users", require("./routes/userRoutes"))
 
-app.listen(5000, () => console.log("Server running on port 5000"))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
